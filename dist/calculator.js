181bc0daa085f68219823557e925df7c8a0426f6
UmairAshraf12 <ashrafumair7778@gmail.com>
2024-02-12 21:07:51 +0500

    Initial commit

---
+++ dist/calculator.js
@@ -0,0 +1,68 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import { sum, sub, mul, div } from "./functions.js";
+import showBanner from "node-banner";
+import gradient from "gradient-string";
+let answers = [
+    {
+        name: "num1",
+        type: "number",
+        message: gradient.rainbow("Enter first number:"),
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please enter number:";
+            }
+            return true;
+        }
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: gradient.rainbow("Enter second number:"),
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please enter number:";
+            }
+            return true;
+        }
+    },
+    {
+        name: "operation",
+        type: "list",
+        choices: ["add", "sub", "mul", "div"],
+        message: gradient.rainbow("Enter operation"),
+    },
+];
+let answer = [
+    {
+        name: "again",
+        type: "confirm",
+        message: "Do you want to calculate again. "
+    }
+];
+(async () => {
+    await showBanner('Calculator', 'This calculator can perform addition,subtraction,multiplication and division', "red", "blue");
+})();
+async function calculator() {
+    let condition = true;
+    while (condition) {
+        let { num2, num1, operation } = await inquirer.prompt(answers);
+        if (operation === "add") {
+            console.log("The sum of two numbers: ", sum(num1, num2));
+        }
+        else if (operation === "sub") {
+            console.log("The difference of two numbers: ", sub(num1, num2));
+        }
+        else if (operation === "mul") {
+            console.log("The multiple of two numbers: ", mul(num1, num2));
+        }
+        else if (operation === "div") {
+            console.log("The division of two numbers: ", div(num1, num2));
+        }
+        let { again } = await inquirer.prompt(answer);
+        condition = again;
+    }
+}
+setTimeout(() => {
+    calculator();
+}, 1000);
